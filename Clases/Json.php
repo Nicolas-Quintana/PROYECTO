6c2fb00169c99b97176248f967f60e0bf3774efc
Edinsonapo <Edinsonapo@icloud.com>
2018-11-05 20:10:41 -0300

    total4

---
+++ Clases/Json.php
@@ -0,0 +1,100 @@
+<?php
+ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
+class Json extends Database {
+    
+    public $archivo;
+
+    public function __construct(String $archivo) {
+        $this->archivo = $archivo;
+    }
+
+    // Guardamos el usuario en la base de datos (json), recibimos como parámetro un array Usuario
+    public function guardarUsuario(User $usuario)
+    {
+
+        $user = [
+            "nombre" => $usuario->getNombre(),
+            "apellido" => $usuario->getApellido(),
+            "email" => $usuario->getEmail(),
+            "password" => $usuario->getPassword(),
+            "fotoPerfil" => $usuario->getFotoPerfil()
+        ];
+
+        $usuarios = $this->traerUsuarios();
+        $usuarios['usuarios'][] = $user;
+
+        // Convertimos el usuario array a json
+        $usuarioJson = json_encode($usuarios);
+        
+        // Guardamos el json a un archivo de texto, agregandole PHP_EOL para que se agregue una nueva linea al final. Le pasamos como parámetro FILE_APPEND para que el texto se agregue y no se pise todo.
+        file_put_contents($this->archivo, $usuarioJson);
+    }
+    
+    // Traemos todos los usuarios de la base de datos.
+    public function traerUsuarios()
+    {
+        $arrayUsuarios = [];
+        
+        // Abrimos el archivo en modo lectura.
+        $archivo = file_get_contents('usuarios.json');
+        $arrayUsuarios = json_decode($archivo, true);
+        
+        return $arrayUsuarios;
+    }
+    
+    // Hacemos lo mismo que en la función anterior, excepto que en este caso, en vez de guardar los usuarios en un array, los vamos leyendo uno por uno hasta encontrar el correcto y lo devolvemos.
+    // En caso de no encontrar nada, devuelve null.
+    public function traerUsuario($email)
+    {
+        $usuarios = $this->traerUsuarios();
+
+        foreach ($usuarios['usuarios'] as $usuario) {
+            if ($usuario['email'] === $email) {
+                return new User($usuario['nombre'],$usuario['apellido'], $usuario['email'], $usuario['password'], $usuario['fotoPerfil']);                
+            }
+        }
+
+        return NULL;
+    }  
+    
+    public function modificarDatos()
+    {
+        $usuarios = $this->traerUsuarios();
+        $usuarioFinal = null;
+
+
+        foreach ($usuarios['usuarios'] as $posicion => $usuario) {
+            if ($usuario['email'] === user()->getEmail()) {
+
+                foreach ($_POST as $campo => $valor) {
+                    if ($valor !== '') {
+                        if ($campo === 'password') {
+                            $usuarios['usuarios'][$posicion][$campo] = password_hash($valor, PASSWORD_DEFAULT);
+                        } else {
+                            $usuarios['usuarios'][$posicion][$campo] = $valor;
+                        }
+                    }
+                }
+
+                $usuarioFinal = new User($usuarios['usuarios'][$posicion]['nombre'],$usuarios['usuarios'][$posicion]['apellido'], $usuarios['usuarios'][$posicion]['email'], $usuarios['usuarios'][$posicion]['password'], $usuarios['usuarios'][$posicion]['fotoPerfil']);
+            }
+        }
+
+        if ($usuarioFinal !== null) {
+            $validacion = Validator::validarRegister($usuarioFinal);   
+
+            if (!$validacion) {
+                file_put_contents($this->archivo, json_encode($usuarios));
+                return $usuarioFinal;
+            } else {
+                return $validacion;
+            }
+        }
+
+    }
+}
+
+
+?>
--- Clases/User.php
+++ Clases/User.php
@@ -1,31 +1,78 @@
 <?php
-
 class User {
     
-private $nombre;
-private $apellido;
-private $email;
-private $password;
-private $fotoPerfil;
+    private $nombre;
+    private $apellido;
+    private $email;
+    private $password;
+    private $fotoPerfil;
 
-
-public function __construct(String $nombre, String $apellido, String $email, String $password, String $fotoPerfil = "")
+    // Creamos el usuario, esta funcion se ejecuta cada vez que se crea un nuevo objeto.
+    public function __construct(String $nombre, String $apellido,String $email, String $password, String $fotoPerfil = null)
     {
-            $this->setUsername($nombre);
-            $this->setUsername($apellido);
+            $this->setNombre($nombre);
+            $this->setApellido($apellido);
             $this->setEmail($email);
             $this->setPassword($password);
             $this->setFotoPerfil($fotoPerfil);
     }
 
-    
+    //estas funciones setean y regresan los atributos de la clase a través de metodos
+    public function getNombre(){
+        return $this->nombre;
+    }
 
+    public function setNombre(String $nombre){
+        $this->nombre = $nombre;
 
+        return $this;
+    }
 
+    public function getApellido(){
+        return $this->apellido;
+    }
 
+    public function setApellido(String $apellido){
+        $this->nombre = $apellido;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(String $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword(String $password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFotoPerfil()
+    {
+        return $this->fotoPerfil;
+    }
+
+    public function setFotoPerfil(String $fotoPerfil)
+    {
+        $this->fotoPerfil = $fotoPerfil;
+
+        return $this;
+    }
 }
-
-
-
 
 ?>
