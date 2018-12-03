39aec60f169e8a62902300417e418592afcd50bd
Edinsonapo <Edinsonapo@icloud.com>
2018-12-03 16:43:01 -0300

    muro1

--- _muro.php
+++ _muro.php
@@ -41,7 +41,7 @@
 			</section>
 
 			<?php 
-				for ($i=0; $i < 10; $i++) { 
+				for ($i=0; $i < 1; $i++) { 
 			?>
 			<section>
 				<div class="avatar">
@@ -49,7 +49,7 @@
 						<img src="img/avatar64.jpg" alt="">
 					</div>
 					<div class="action">
-						<h2><a href="_profile.php">Benito Camelo Parado Al Fondo</a></h2>
+						<h2><a href="_profile.php">nombre</a></h2>
 						<h3> <a href="#">Hace dos horas</a></h3>
 					</div>
 					<div style="clear:both;"></div>
@@ -62,7 +62,7 @@
 				<div class="like_tools">
 					<a href="#" class="like_button">Me gusta</a>
 					<a href="#" class="dislike_button">No me gusta</a>
-					<a href="#openmodal" class="comments_button">25 Comentarios</a>
+					<a href="" class="comments_button">25 Comentarios</a>
 				</div>
 			</section>
 			<?php
@@ -102,61 +102,7 @@
 
 		<div style="clear:both;"></div>
 	</div>
-	<div class="background_modal" id="openmodal">
-		<div class="modal_container">
-			<a href="#close" class="close_button"><img src="img/close.png" alt=""></a>
-			<div class="modal_content">
-				<section>
-				<div class="avatar">
-					<div class="background_image">
-						<img src="img/avatar64.jpg" alt="">
-					</div>
-					<div class="action">
-						<h2><a href="_profile.php">Benito Camelo Parado Al Fondo</a></h2>
-						<h3> <a href="#">Hace dos horas</a></h3>
-					</div>
-					<div style="clear:both;"></div>
-				</div>
-				<div class="status">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur exercitationem sit dolores recusandae, reprehenderit atque eos quod excepturi sunt mollitia maxime corrupti, fuga modi vel non architecto quos perspiciatis aperiam? iciatis aperiam
-					</p>
-				</div>
-				<div class="like_tools">
-					<a href="#" class="like_button">Me gusta</a>
-					<a href="#" class="dislike_button">No me gusta</a>
-					<a href="#openmodal" class="comments_button">25 Comentarios</a>
-				</div>
-				<div class="comments_container">
-				<?php for ($i=0; $i < 15; $i++) { ?>
-					<div class="comment">
-						<div class="image">
-							<img src="img/avatar64.jpg" width="35" alt="Imagen">
-						</div>
-						<div class="text">
-							<a href="#" class="name">Soila del Hoyo Prieto</a>
-								Lorem ipsum dolor sit amet, consectetur adipisicing.
-						</div>
-						<div style="clear:both;"></div>
-					</div>
-				<?php } ?>
-				<!-- ========== caja comentario ===========  -->
-					<div class="comment">
-						<div class="image">
-							<img src="img/avatar64.jpg" width="35" alt="Imagen">
-						</div>
-						<div class="text">
-							<form method="post" action="#">
-								<input type="text" name="comment" placeholder="Haz un comentario">
-							</form>
-						</div>
-						<div style="clear:both;"></div>
-					</div>
-				</div>
-			</section>
-			</div>
-		</div>
-	</div>
+	
 	
 </body>
 </html>
